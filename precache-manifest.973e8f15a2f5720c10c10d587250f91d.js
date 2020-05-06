7d8d99b35fa4c44ab0aa80724bd037395485279b
Danis Desipris <danis_desipris@hotmail.com>
2020-05-06 17:27:15 +0200

    Updates

---
+++ precache-manifest.973e8f15a2f5720c10c10d587250f91d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8067b504f947affb3ec2cb8019946aa4",
+    "url": "/randomUser/index.html"
+  },
+  {
+    "revision": "a436494921c85b568adb",
+    "url": "/randomUser/static/js/2.604fb443.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/randomUser/static/js/2.604fb443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca0534051ee30950985",
+    "url": "/randomUser/static/js/main.125efcf7.chunk.js"
+  },
+  {
+    "revision": "01c73526bfe8d1a04147",
+    "url": "/randomUser/static/js/runtime-main.925228ac.js"
+  }
+]);
